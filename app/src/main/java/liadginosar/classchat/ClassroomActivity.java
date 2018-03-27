80db0a1c9f05fa193d0303681e7843bfb30113c2
Liad Ginosar <liadginosar@gmail.com>
2018-03-27 17:04:22 +0300

    adding firebase

--- app/src/main/java/liadginosar/classchat/ClassroomActivity.java
+++ app/src/main/java/liadginosar/classchat/ClassroomActivity.java
@@ -5,7 +5,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class ClassroomActivity extends AppCompatActivity {
+
+    private FirebaseDatabase mFirebaseDatabase;
+    private DatabaseReference mFirebaseRef;
+
+
+
+    private static final String CLASSROOM_PATH = "/classroom/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +28,10 @@ public class ClassroomActivity extends AppCompatActivity {
         TextView title = findViewById(R.id.textViewClassroomTitle);
 
         title.setText(classroom);
+
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+        mFirebaseRef = mFirebaseDatabase.getReference(CLASSROOM_PATH);
+
+
     }
 }
